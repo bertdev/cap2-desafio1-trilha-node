4db238ca8d40b6c35da7afa2fe61afef64e98425
bert <herbertmatos10@hotmail.com>
2021-12-05 17:30:45 -0300

    feat: implementa a funcionalidade de transformar um user em admin

--- src/modules/users/useCases/turnUserAdmin/TurnUserAdminController.ts
+++ src/modules/users/useCases/turnUserAdmin/TurnUserAdminController.ts
@@ -3,10 +3,16 @@ import { Request, Response } from "express";
 import { TurnUserAdminUseCase } from "./TurnUserAdminUseCase";
 
 class TurnUserAdminController {
-  constructor(private turnUserAdminUseCase: TurnUserAdminUseCase) {}
+  constructor(private turnUserAdminUseCase: TurnUserAdminUseCase) { }
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    const { user_id } = request.params;
+    try {
+      const admin = this.turnUserAdminUseCase.execute({ user_id });
+      return response.status(200).json(admin);
+    } catch (e) {
+      return response.status(400).json({ error: e.message });
+    }
   }
 }
 
